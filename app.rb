224f9995f525c5cc01a71a24cebb5dbbea37cb0d
Kathleen French <kfrench09@gmail.com>
2017-10-22 11:42:24 -0400

    call to api, display temperatures

--- app.rb
+++ app.rb
@@ -5,6 +5,11 @@ ConfigEnv.init("#{__dir__}/config/env.rb")
 set :port, 3000
 
 get '/' do
-  @api_key = ENV['API_KEY']
+  resp = HTTParty.get(
+    "http://api.openweathermap.org/data/2.5/weather?q=London&APPID=#{ENV['API_KEY']}"
+  )
+  data = JSON.parse(resp.body)
+  # convert temp from kelvin to Farenheit
+  @temp = data['main']['temp'] * 9 / 5 - 459.67
   erb :index
 end
